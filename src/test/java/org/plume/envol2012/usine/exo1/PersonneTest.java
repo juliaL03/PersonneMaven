2f789134cbb74a65009a95a3fcb1031b87eb79f4
juliaL03 <julia.leven@etudiant.univ-lille1.fr>
2015-10-26 23:11:46 +0100

    Deuxième Test

--- src/test/java/org/plume/envol2012/usine/exo1/PersonneTest.java
+++ src/test/java/org/plume/envol2012/usine/exo1/PersonneTest.java
@@ -10,8 +10,21 @@ public class PersonneTest extends TestCase {
 	public void testConstructeurPersonne() {
 		final String NOM = "ROUSSE";
 		final String PRENOM = "David";
-		Personne personne = new Personne(NOM, PRENOM);
+		final int AGE = 23;
+		Personne personne = new Personne(NOM, PRENOM, AGE);
 		assertEquals(NOM, personne.getNom());
 		assertEquals(PRENOM, personne.getPrenom());
+	}
+	
+	@Test
+	public void testConstructeurPersonneAge() {
+		final String NOM = "ROUSSE";
+		final String PRENOM = "David";
+		final int AGE = 23;
+		Personne personne = new Personne(NOM, PRENOM, AGE);
+		assertEquals(NOM, personne.getNom());
+		assertEquals(PRENOM, personne.getPrenom());
+		assertEquals(AGE,23);
+		
 	}
 }
